ed142ed9d42f84ab54f4b4d11f2cfe88c8a0d08d
Duncan Murray <djmurray@acutesoftware.com.au>
2015-08-23 00:25:36 +0930

    scripts to run all tests and coverage

---
+++ tests/run_tests.py
@@ -0,0 +1,23 @@
+# run_tests.py
+import os
+import time
+import unittest as unittest
+
+all_tests = unittest.TestLoader().discover('.', pattern='test*.py')
+unittest.TextTestRunner().run(all_tests)    
+
+def wipe_file(fname):
+    if os.path.exists(fname):
+        try:
+            os.remove(fname)
+            print('deleted ' + fname)
+        except:
+            pass
+        
+
+print ('WIPING ALL TEST RESULTS - PRESS CTRL C TO STOP')
+
+time.sleep(10)
+
+wipe_file('test_world.txt')
+wipe_file('world_traversed.txt')
