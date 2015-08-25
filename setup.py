4c1b220fef344c853ffb3e5560f78ae9906257cf
Duncan Murray <djmurray@acutesoftware.com.au>
2015-08-25 21:44:12 +0930

    setup.py has license as string and ready for next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='worldbuild',
-    version='0.0.1',
+    version='0.0.2',
     author='Duncan Murray',
     author_email='djmurray@acutesoftware.com.au',
     packages=['worldbuild', 'worldbuild.data'],
@@ -10,13 +10,13 @@ setup(
     install_requires=[
           'nose >= 1.0',
           'noise >= 1.2.1',
-          'aikif >= 0.1.3'
+          'aikif >= 0.1.4'
     ],
     include_package_data = True,
     package_data = {
         'worldbuild': ['data/*.*'],
     },   
-    license='LICENSE.txt',
+    license='GNU General Public License v3 (GPLv3)',
     description='Tools for computational world building',
     long_description=open('README.rst').read(),
     classifiers = [
