5ea446c3405fb1668038a7c0283d7cf6606197d6
Duncan Murray <djmurray@acutesoftware.com.au>
2015-09-16 00:02:46 +0930

    playing with extracting colour histogram from image

--- worldbuild/convert_map_to_data.py
+++ worldbuild/convert_map_to_data.py
@@ -7,11 +7,26 @@ import config
 import sample
 import pprint
 import shutil
+import operator
+
 import html_utils
 import image_utils
 import aikif.toolbox.image_tools as mod_img
 
+def trace(fn):
 
+    def build_trace_line(fn, args, ret_val):
+        return "   " + fn.__name__ + str(args) + " -> " + str(ret_val) + "\n"
+
+    def wrapper(*args):
+        ret_val = fn(*args)
+        trace_line = build_trace_line(fn, args, ret_val)
+        print(trace_line.ljust(10))
+        return ret_val
+
+    return wrapper
+
+@trace
 def extract_data_from_map(fname):
     """
     read in a jpg file containing a map and attempt to 
@@ -33,6 +48,20 @@ def extract_data_from_map(fname):
             all_pixels.append(cpixel)
     
     print(all_pixels[0:10])
+    
+    # get a histogram of colours
+    
+    colours = {}
+    for p in all_pixels:
+        if p in colours:
+            colours[p] += 1
+        else:
+            colours[p] = 1
+    
+    sorted_x = sorted(colours.items(), key=operator.itemgetter(1))
+    for k, v in sorted_x:
+        if v > 100:
+            print(k,v)
     
     # identify sea - blue
     
