8c1991046080a93b8ca38bf573a78780f5fe9b75
Duncan Murray <djmurray@acutesoftware.com.au>
2016-06-17 22:57:05 +0930

    test for Minecraft with variable delays timed

--- tests/test_cls_interface_application.py
+++ tests/test_cls_interface_application.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import unittest
+import time
 
 root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
 pth = root_folder + os.sep + 'worldbuild'
@@ -20,11 +21,23 @@ class TestAgentInterfaceEnvironment(unittest.TestCase):
         self.assertEqual(str(res),'Base class Interface for MINECRAFT')
         
     def test_02_activate_minecraft(self):
+        start_time = time.time()
         res = mod_if.InterfaceMineCraft('Minecraft server')
         res.activate()
+        return_val = res.send_command('/say test_02')
+        self.assertEqual(return_val, '/say test_02{ENTER}')
+        self.assertTrue((time.time() - start_time) < 1)
         
-        return_val = res.send_command('/say hello')
-        self.assertEqual(return_val, '/say hello{ENTER}')
+    def test_03_slow_delay(self):
+        start_time = time.time()
+        res = mod_if.InterfaceMineCraft('Minecraft server', time_delay=2)
+        res.activate()
+        return_val = res.send_command('/say test_03')
+        self.assertEqual(return_val, '/say test_03{ENTER}')
+        end_time = time.time()
+        print('Time for delayed send_keys = ', end_time - start_time)
+        self.assertTrue((end_time - start_time) > 3)
+        self.assertTrue((end_time - start_time) < 5)
         
 if __name__ == '__main__':
     unittest.main()
