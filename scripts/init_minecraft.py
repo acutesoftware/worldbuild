cf3c6c7abb05cb2ca473e550d892cf65bf4d7ff8
Duncan Murray <djmurray@acutesoftware.com.au>
2016-06-18 22:19:44 +0930

    init_minecraft uses class for simpler control

--- scripts/init_minecraft.py
+++ scripts/init_minecraft.py
@@ -1,8 +1,20 @@
 # init_minecraft.py
+import os
+import sys
 
-import time
-import aikif.toolbox.interface_windows_tools as mod_tool
+sys.path.append(os.path.join('..','worldbuild'))
+import cls_interface_application as mod_if
 
+mc = mod_if.InterfaceMineCraft('Minecraft server', 0.2)
+mc.activate()
+print(mc.send_command('/difficulty 2'))
+print(mc.send_command('/op DynamiteBuilder'))
+print(mc.send_command('/op craftandstore'))
+
+
+
+
+"""
 print('remember to start the minecraft server')
 mod_tool.app_activate('Minecraft server')
 time.sleep(1.0) 
@@ -14,6 +26,7 @@ time.sleep(0.2)
 mod_tool.send_keys("{ENTER}")  # needs Enter key
 time.sleep(0.2) 
 
+
 players = ['DynamiteBuilder', 'craftandstore']
 
 for p in players:
@@ -21,3 +34,4 @@ for p in players:
     mod_tool.send_keys("{ENTER}")  # needs Enter key
     time.sleep(0.1) 
     
+"""
