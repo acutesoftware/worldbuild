dc47f22bddc8a9251826f551a75186470c34467a
Duncan Murray <djmurray@acutesoftware.com.au>
2016-07-21 00:16:39 +0930

    minecraft builder lib has function to make from list as well as file

--- scripts/minecraft/minecraft_builder.py
+++ scripts/minecraft/minecraft_builder.py
@@ -10,23 +10,31 @@ def main():
 def make_structure(fname):
     print('NOTE - you need to set focus to the command textbox in the Minecraft server')
     print('and do NOT lose focus to the server (dont touch or select anything while this runs)')
-
+    res = []
     with open(fname, 'r') as f:
         for line in f:
-            if line.strip('\n') == '':
-                pass
-            elif line[0:1] == '#':
-                pass
-            elif line[0:1] == '@':
-                mod_tool.app_activate(line.strip('\n')[1:])
-                time.sleep(1)     
+            res.append(line)
+    make_from_list(res)
                 
-            else:
-                print('sending keys ' , line.strip('\n'))
-                mod_tool.send_keys(line.strip('\n'))
-                time.sleep(0.01) 
-                mod_tool.send_keys("{ENTER}")
-                time.sleep(0.1) 
+                
+def make_from_list(lst):
+    for line in lst:
+        if line.strip('\n') == '':
+            pass
+        elif line[0:1] == '#':
+            pass
+        elif line[0:1] == '@':
+            mod_tool.app_activate(line.strip('\n')[1:])
+            time.sleep(1)     
+            
+        else:
+            print('sending keys ' , line.strip('\n'))
+            mod_tool.send_keys(line.strip('\n'))
+            time.sleep(0.01) 
+            mod_tool.send_keys("{ENTER}")
+            time.sleep(0.1) 
+    
 
+                
 if __name__ == '__main__':                
     main()            
