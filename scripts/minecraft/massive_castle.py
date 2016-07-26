423856273a9819c44f2c5dd0cf922891949260d4
Duncan Murray <djmurray@acutesoftware.com.au>
2016-07-26 20:29:30 +0930

    works - makes a large grounds and basic building areas

--- scripts/minecraft/massive_castle.py
+++ scripts/minecraft/massive_castle.py
@@ -27,19 +27,21 @@ import clear_area
 """
 
 #clear_area.wipe_all(x = 28, y = 63, z = 25, w = 275, h = 15, d = 150)
-
+# make_castle_walls(start_x, start_y, start_z, width, height, length, wall_width)
 
 # make outer walls (make a BIG square, a smaller square, then wipe the corners to match
-castle_maker.make_castle_walls(30,63,30,120,4,90, 6)  # massive outer wall
-#castle_maker.make_castle_walls(70,63,30,40,4,26, 6)  # gate inset wall
+castle_maker.make_castle_walls(30,63,30,120,4,90, 6)  # TOK massive outer wall
 
-castle_maker.make_castle_walls(30,63,75,20,4,45, 6)  # lower left inset wall
-castle_maker.make_castle_walls(130,63,75,20,4,45, 6)  # lower right inset wall
+castle_maker.make_castle_walls(70,63,30,40,4,20, 6)  # TOK gate inset wall
+castle_maker.make_castle_walls(30,63,74,20,4,46, 6)  # TOK lower left inset wall
+castle_maker.make_castle_walls(130,63,74,20,4,46, 6)  # TOK lower right inset wall
 
 
-clear_area.wipe_all(x = 30, y = 63, z = 78, w = 12, h = 7, d = 37)  # clear bottom left corner
-clear_area.wipe_all(x = 137, y = 63, z = 78, w = 12, h = 7, d = 37) # clear bottom right corner
-clear_area.wipe_all(x = 79, y = 63, z = 30, w = 25, h = 7, d = 9) # clear front gate inset
+clear_area.wipe_all(x = 30, y = 63, z = 81, w = 10, h = 7, d = 49)  # TOK clear bottom left corner
+
+clear_area.wipe_all(x = 137, y = 63, z = 81, w = 13, h = 7, d = 49) # clear bottom right corner
+
+clear_area.wipe_all(x = 77, y = 63, z = 30, w = 22, h = 7, d = 8) # clear front gate inset
 
 
 
@@ -49,6 +51,12 @@ castle_maker.make_castle_walls(130,63,35,14,9,10, 2)  # top right tower
 castle_maker.make_castle_walls(70,63,75,40,7,30, 2)  # main building 
 castle_maker.make_castle_walls(55, 63,100,6,12, 6, 0)  # bottom left tower
 castle_maker.make_castle_walls(120,63,100,6,12,6, 0)  # bottom right tower
+
+
+
+
+
+
 
 
 
