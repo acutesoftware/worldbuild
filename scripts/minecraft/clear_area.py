73b5a7bf665751f938acc41443d4667d6f61c1a1
Duncan Murray <djmurray@acutesoftware.com.au>
2016-07-27 00:41:51 +0930

    modified clear_area to wipe tall towers

--- scripts/minecraft/clear_area.py
+++ scripts/minecraft/clear_area.py
@@ -4,15 +4,20 @@ import minecraft_builder as mcb
 
 
 def TEST():
-    #wipe_all(x = 30-2, y = 63-2, z = 30-2, w = 100, h = 15, d = 100)
-    wipe_all(x = 28, y = 63, z = 25, w = 275, h = 15, d = 150)
+    print('clearing castle area')
+    # TOK - clears all walls (all inner area of castle 
+    #wipe_all(x = 28, y = 63, z = 25, w = 275, h = 75, d = 150)
+    wipe_all(x=72, y=63, z=77,w=36,h=30,d=26)
+    wipe_all(x=70, y=74, z=70,w=46,h=50,d=36)
+
+    wipe_all(x=30, y=74, z=30,w=46,h=50,d=20)  # 37,96,36,11,3,8, 0)  
     
 def wipe_all(x,y,z,w,h,d):
     res = []
     res.append('@Minecraft Server')
     res.append('/say hello from server, just clearing the area')
-    for n in range(x, x + w, 5):
-        res.append(mcb.mc_fill(n, y, z, n+5, y+h, z+d, 'minecraft:air 0'))
+    for n in range(x, x + w, 1):
+        res.append(mcb.mc_fill(n, y, z, n+1, y+h, z+d, 'minecraft:air 0'))
     mcb.make_from_list(res)
     
 
