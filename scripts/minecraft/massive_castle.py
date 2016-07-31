92d08891e3ce7526fe079da26b90ba976353ea84
Duncan Murray <djmurray@acutesoftware.com.au>
2016-07-31 23:17:54 +0930

    massive_castle includes catapults, towers, horse stables (no horse spawn yet)

--- scripts/minecraft/massive_castle.py
+++ scripts/minecraft/massive_castle.py
@@ -27,10 +27,67 @@ import clear_area
 """
 
 def main():
-    do_garden()
-    do_walls()
-    do_castle()
-    do_towers()
+    #do_garden()
+    #do_walls()
+    #do_castle()
+    #do_towers()
+    #defend_walls()
+    #do_vertical_garden()
+    stables()
+
+def stables():    
+    # set the ground
+    castle_maker.fill_area(115,60,35,129,62,43, 'minecraft:grass') # sunken stables
+    castle_maker.fill_area(115,61,33,129,65,39, 'minecraft:planks 2 hollow') # sunken stables
+    castle_maker.fill_area(116,62,36,128,64,42, 'minecraft:air') # sunken stables
+    castle_maker.fill_area(115,63,40,115,63,46, 'minecraft:fence') 
+    castle_maker.set_block(130,63,46, 'fence_gate 1')
+    castle_maker.set_block(128,63,71, 'fence_gate')
+    castle_maker.set_block(115,63,46, 'fence_gate 1')
+    
+    castle_maker.set_block(117,64,34, 'minecraft:torch 3') 
+    castle_maker.set_block(122,64,34, 'minecraft:torch 3') 
+    castle_maker.set_block(128,64,34, 'minecraft:torch 3') 
+    
+    castle_maker.set_block(120,62,34,'hay_block')
+    castle_maker.set_block(118,62,35,'hay_block')
+    castle_maker.set_block(123,61,41,'water')
+    #castle_maker.set_block(121,62,40,'mob_spawner 0 replace {SpawnData:{id:EntityHorse}}')  # /setblock ~ ~ ~ mob_spawner 0 replace {SpawnData:{id:Zombie}}
+    #castle_maker.set_block(121,62,40,'mob_spawner {SpawnData:{id:Horse}}')  # 	/setblock ~ ~ ~ mob_spawner 0 replace {SpawnData:{id:Zombie}}
+    #  /setblock ~ ~ ~ mob_spawner 0 replace {id:EntityHorse, SpawnData: {Type:3, Tame:1}}
+    # /summon EntityHorse 121 62 40 {Type:4,Tame:1}
+    # /setblock 121 62 40 mob_spawner 0 replace {SpawnData:{id:EntityHorse}}   <-- this works in server, but leaves cage??
+
+    
+    # {Tame:1,Saddle:1,Attributes:[{Name:generic.movementSpeed,Base:1},{Name:horse.jumpStrength,Base:2}]}
+    #castle_maker.set_block(121,62,40,'mob_spawner 0 replace {SpawnData:{id:EntityHorse},{Tame:1,Saddle:1,Attributes:[{Name:generic.movementSpeed,Base:1},{Name:horse.jumpStrength,Base:2}]} }') 
+    
+    # SERVER COMMAND - works
+    # /setblock 121 62 40 mob_spawner 0 replace {SpawnData:{id:EntityHorse}}
+    
+    
+    # Chickens and Cows on West side
+    castle_maker.fill_area(66,63,36,66,63,47, 'minecraft:fence') 
+    castle_maker.set_block(49,63,46, 'fence_gate 1')
+    castle_maker.set_block(51,63,47, 'fence_gate')
+    castle_maker.set_block(65,63,47, 'fence_gate')
+    
+    
+    
+    
+    
+    
+def do_vertical_garden():    
+    # set the ground
+    castle_maker.fill_area(83,113,80,94,113,89, 'minecraft:glowstone')
+    castle_maker.fill_area(84,113,81,93,113,88, 'minecraft:grass')
+    castle_maker.set_block(87, 114, 83,'minecraft:sapling 2')
+    castle_maker.set_block(87, 114, 86,'minecraft:sapling 2')
+    castle_maker.set_block(91, 114, 83,'minecraft:sapling 2')
+    castle_maker.set_block(91, 114, 86,'minecraft:sapling 2')
+    castle_maker.set_block(90, 113, 82,'minecraft:water')
+    castle_maker.set_block(90, 113, 86,'minecraft:water')
+    
     
 def do_garden():    
     # set the ground
@@ -69,6 +126,44 @@ def do_garden():
     castle_maker.plant(x1=129,z1=47,x2=141,z2=59,y1=62, item='minecraft:carrots 4') # 'wheat', 
     castle_maker.plant(x1=129,z1=59,x2=141,z2=71,y1=62, item='minecraft:pumpkin 4') # 'wheat', 
 
+def defend_walls():
+    """
+    #castle_maker.catapult(x=80,y=63, z=30)
+    """
+    castle_maker.catapult(58,68,31) # top of front wall - East of gate
+    castle_maker.catapult(x=90,y=70, z=45) # top of front gate
+    castle_maker.catapult(122,68,31) # top of front wall - West of gate
+    
+    castle_maker.catapult(103, 111, 79) # top of main bld - East
+    castle_maker.catapult(76, 111, 79)  # top of main bld - West
+    
+    castle_maker.catapult(42, 98, 38)  # top of front left tower
+    castle_maker.catapult(137, 98, 38)  # top of front right tower
+    castle_maker.catapult(126, 98, 108) # South West tower
+
+    castle_maker.catapult(54, 98, 107) # South East tower
+    
+    # tok - lights on west wall going up 
+    castle_maker.stairs_NS(130, 93, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    
+    
+    castle_maker.stairs_NS(111, 40, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    
+    
+    # stairs on walls - NS  110, 63->67, 30     130, 68 -> 72, 37    144, 63-67, 59   129, 63-67, 28 (outside)
+
+    #stairs_NS(x, z, width, y_base, y_top, step='minecraft:stone 4', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(111, 40, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(130, 35, 1, 68, 73, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(144, 59, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(129, 78, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+
+    #stairs - west of gate 
+    castle_maker.stairs_NS(70, 40, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(48, 35, 1, 68, 73, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(35, 59, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    castle_maker.stairs_NS(49, 95, 1, 63, 68, step='minecraft:stone_brick_stairs 2', bannister='minecraft:air', step_spacing=1)
+    
 
 def do_walls():
     # make outer walls (make a BIG square, a smaller square, then wipe the corners to match
@@ -83,7 +178,8 @@ def do_walls():
 
     castle_maker.gate(x=86, y=63, z=43, width=8, height=8, length=8)
     
-    # undercut on outer wall perimeter to stop spiders
+    # undercut on outer wall perimeter to stop spiders - may not need this soon TOD - remove SOME of this , NOT INTENTS
+    """
     castle_maker.fill_area(30,63,30,150,66,30, 'minecraft:air')   # front wall across very front
     castle_maker.fill_area(76,63,30,76,66,44, 'minecraft:air')    # lhs inset near gate
     castle_maker.fill_area(104,63,30,104,66,44, 'minecraft:air')  # rhs inset near gate
@@ -98,7 +194,7 @@ def do_walls():
     castle_maker.fill_area(30,63,120,150,66,120, 'minecraft:air') # back wall - very back
     castle_maker.fill_area(30,63,80,44,66,80, 'minecraft:air')    # lhs inset
     castle_maker.fill_area(136,63,80,150,66,80, 'minecraft:air')  # rhs inset
-
+    """
 
 def do_castle():
 
@@ -134,11 +230,11 @@ def do_castle():
     
     castle_maker.main_door(x=90, y=63, z=75)  # coords for door are bottom centre - ornate stuff built out from there
 
-    castle_maker.stairs_NS(x=88, z=87, width=4, y_base=64, y_top=74, step='minecraft:quartz_stairs 2', bannister='minecraft:quartz_block 2', step_spacing=1)
+    castle_maker.stairs_NS(x=88, z=87, width=4, y_base=64, y_top=75, step='minecraft:quartz_stairs 2', bannister='minecraft:quartz_block 2', step_spacing=1)
 
     # 2nd floor
-    castle_maker.stairs_NS(x=74, z=90, width=2, y_base=74, y_top=87, step='minecraft:stone 4', bannister='minecraft:stone 4', step_spacing=1)
-    castle_maker.stairs_NS(x=101, z=90, width=2, y_base=74, y_top=87, step='minecraft:stone 4', bannister='minecraft:stone 4', step_spacing=1)
+    castle_maker.stairs_NS(x=74, z=90, width=2, y_base=75, y_top=87, step='minecraft:stone 4', bannister='minecraft:stone 4', step_spacing=1)
+    castle_maker.stairs_NS(x=101, z=90, width=2, y_base=75, y_top=87, step='minecraft:stone 4', bannister='minecraft:stone 4', step_spacing=1)
     
     # 3rd floor 90,86,90 --> 98 
     castle_maker.stairs_NS(x=90, z=85, width=2, y_base=87, y_top=99, step='minecraft:quartz_stairs 2', bannister='minecraft:quartz_block 2', step_spacing=1)
