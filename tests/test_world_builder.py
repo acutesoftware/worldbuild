872ecba6d17fc224b1c9676b4525e0d0d4b3ac51
Duncan Murray <djmurray@acutesoftware.com.au>
2016-09-02 00:09:31 +0930

    detailed test for world_builder passing structure definition

--- tests/test_world_builder.py
+++ tests/test_world_builder.py
@@ -13,7 +13,6 @@ sys.path.append(pth)
 
 import world_builder
 
-
 structure_data = [{'name':'test_world_build', 'coords':[0,5,67,3,6]}]
 style_data = {'base':'wood', 'colour':'greens', 'texture':'soft'}
 
@@ -21,9 +20,12 @@ style_data = {'base':'wood', 'colour':'greens', 'texture':'soft'}
 class TestWorldBuilder(unittest.TestCase):
     def test_01_instantiate_base_class(self):
         res = world_builder.BuildMap(structure_data,style_data)  # make sure we can pass dict or list
-        print(res)
-        #self.assertEqual(str(res),'Base class Interface for MINECRAFT')
+        self.assertEqual(res.style['base'],'wood')
+        self.assertEqual(res.style['colour'],'greens')
+        self.assertEqual(res.style['texture'],'soft')
 
+        self.assertEqual(res.struct_data[0]['name'],'test_world_build')
+        self.assertEqual(res.struct_data[0]['coords'],[0, 5, 67, 3, 6])
         
 
 if __name__ == '__main__':
