5d2e0264b1a069a85959262a9db850df67bdf8bf
Duncan Murray <djmurray@acutesoftware.com.au>
2017-01-13 22:37:14 +1030

    changed build file to use rcon by calling connection at start of script

--- scripts/minecraft/medium_castle.py
+++ scripts/minecraft/medium_castle.py
@@ -13,18 +13,21 @@ d = 33
 
 def main():
     
+    myrcon = castle_maker.rcon_connection()
     clear_area.wipe_all(x-1, y, z-1, w+3, h+50, d+3)  # TOK clear bottom left corner
 
     castle_maker.fill_area(x,y,z,x+w,y,z+d, 'minecraft:grass')
     
     # make_castle_walls(start_x, start_y, start_z, width, height, length, wall_width):
-    castle_maker.make_castle_walls(x,y,z,w,h,d, 4)  # outer wall
+    castle_maker.make_castle_walls(x,y,z,w,h,d, 4, myrcon)  # outer wall
+    
+    """
     castle_maker.gate(1239, y+1, -1925, width=7, height=9, length=6)
     
     castle_maker.tower_building(x+3, y+0, z+6, width=25, height=8, length=21, butt_height=6)
     castle_maker.tower_building(x+3, y+9, z+6, width=25, height=8, length=21, butt_height=7)
  
     castle_maker.make_castle_walls(x+3, y+16,z+6, 25, 4,21, 1)  # TOK 
- 
+    """
     
 main()
