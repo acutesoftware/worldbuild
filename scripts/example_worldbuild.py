a19a151f43db875a9710a869129cfeab3b820adc
acutesoftware <djmurray@acutesoftware.com.au>
2019-04-27 17:30:55 +0930

    script showing dungeon generator after pip install

---
+++ scripts/example_worldbuild.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# example_worldbuild.py
+# Testing worldbuild modules outside of dev environment
+# Run the following command to get the package
+#      pip3 install --user worldbuild
+# Then run this script to test
+
+#import worldbuild.pathfind as pathfind
+
+import worldbuild.dungeon_generator as dg
+
+grid = dg.create_dungeon(grid_y=30, grid_x=80, NUM_ROOMS=16, ROOM_SIZE = 3, NUM_HORIZ = 10)
+print(dg.grid_as_str(grid))
+
+# optional - make a path through the grid
+print(dg.path_find(grid))
+
+# optional - export as TMX file
+dg.convert_grid_to_TileEditor_map('dungeon.tmx', 'samples/ascii_runeset.tsx')
