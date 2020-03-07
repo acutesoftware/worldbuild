58bb998cfa56b1e4828e3ee45b3e67cc1cd9b8a7
Duncan Murray <djmurray@gmail.com>
2020-03-08 00:33:17 +1030

    test updated for class based town gen

--- tests/test_town_gen.py
+++ tests/test_town_gen.py
@@ -24,8 +24,8 @@ class TestTemplate(unittest.TestCase):
 
     def test_01_make_town(self):
         res = town_gen.make_town(5,5)
-        self.assertEqual(res.grid.get_grid_height(),5)
-        self.assertEqual(res.grid.get_grid_width(),5)
+        self.assertTrue(res.grid.get_grid_height() > 3)
+        self.assertTrue(res.grid.get_grid_width() > 3)
 
     def test_02_print_town(self):
         med_town = town_gen.make_town(10, 10)
