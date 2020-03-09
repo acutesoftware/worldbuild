acdfa48353f3a6b0935757a742078e20d9dfb091
Duncan Murray <djmurray@gmail.com>
2020-03-09 17:42:36 +1030

    quest generator working with simple example

--- worldbuild/quest_gen/README.md
+++ worldbuild/quest_gen/README.md
@@ -3,10 +3,74 @@
 prototype code to manage stories and generate quests for games / worlds.
 
 ## Quick Start
-See tests folder for examples
+See tests folder for examples (todo later)
 
-test_quest_story.py = shows how to parse TXT stories into data structures
-test_quest_generate.py = shows how to generate quest code
+For now, run quest.py to generate random quests based on NPC's
+
+```
+
+NPCs in this land:
+Jim is at forest. Status = IdleThis NPC needs : wood
+Sandy is at hills. Status = huntingThis NPC needs : string, shells
+Your Quest List:
++------------------------------------------------------------
+| ***Collect wood ***
+| Jim needs you to collect wood
+| Location = forest
+| Status = Available
+| Reward = 10 Gold
+| Return to Jim with 25 wood
++------------------------------------------------------------
+
++------------------------------------------------------------
+| ***Collect shells ***
+| Sandy needs you to collect shells
+| Location = beach
+| Status = Available
+| Reward = fishing rod
+| Return to Sandy with 25 shells
++------------------------------------------------------------
+
+```
+
+## Quest Generation Process
+
+There are 3 steps to generate quests
+1. Create Locations
+
+2. Create Items
+
+3. Create NPC's
+
+4. Generate Quests
+
+
+### Create Locations
+
+    hills = Location('hills', 'rolling hills', [4,8])
+    forest = Location('forest', 'Dark forest near the hills', [4,7])
+    woods = Location('woods', 'Woodland between mountains and river', [7,12])
+    beach = Location('beach', 'Nice sandy beach', [2,1])
+
+### Create Items
+
+    wood = Item('wood', 'Wooden planks', [forest, hills, woods ])
+    string = Item('string', 'Used for fishing rods and bows', [hills ])
+    shells = Item('shells', 'Shells', [beach ])
+
+### Create NPC's
+
+    jim = NPC('Jim', forest, 'Idle', [wood])                
+    sandy = NPC('Sandy', hills, 'hunting', [string, shells])
+
+### Generate Quests
+    
+    my_quests = []
+    my_quests.append(Quest().create_next_quest_via_npc_needs(jim))
+    my_quests.append(Quest().create_next_quest_via_npc_needs(sandy))
+    
+
+
 
 ## Stories
 Write text files (one file per story section) to build up a story
--- worldbuild/quest_gen/quest.py
+++ worldbuild/quest_gen/quest.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # quest.py
 
+import random
+
 objects = [
     {'name':'rusty key', 
      'desc':'an old rusty key',
@@ -30,14 +32,139 @@ characters = [
 ]
 
 def main():
+    """
     for loc in locations:
         print('you search ' + loc['name'])
         for obj in objects:
             if obj['location'] == loc['name']:
                 print('you find ', obj['name'])
                 print(obj['when_seen'])
+    """
+
+    # setup locations
+    hills = Location('hills', 'rolling hills', [4,8])
+    forest = Location('forest', 'Dark forest near the hills', [4,7])
+    woods = Location('woods', 'Woodland between mountains and river', [7,12])
+    beach = Location('beach', 'Nice sandy beach', [2,1])
 
 
+    wood = Item('wood', 'Wooden planks', [forest, hills, woods ])
+    string = Item('string', 'Used for fishing rods and bows', [hills ])
+    shells = Item('shells', 'Shells', [beach ])
+
+    jim = NPC('Jim', forest, 'Idle', [wood])                
+    sandy = NPC('Sandy', hills, 'hunting', [string, shells])
+
+
+    # generate quest list
+    my_quests = []
+    #q1 = Quest().create_next_quest_via_npc_needs(jim)
+    my_quests.append(Quest().create_next_quest_via_npc_needs(jim))
+
+    #q2 = Quest().create_next_quest_via_npc_needs(sandy)
+    my_quests.append(Quest().create_next_quest_via_npc_needs(sandy))
+    
+    # Display game
+    print('NPCs in this land:')
+    print(jim)
+    print(sandy)
+
+    print('Your Quest List:')
+    for q in my_quests:
+        print(q)
+
+
+class Location(object):
+    """
+    map areas
+    """
+    def __init__(self, name, desc, coords):
+        self.name = name
+        self.desc = desc
+        self.coords =  coords
+    def __str__(self):
+        res = ''
+        res += self.name + ' - ' + self.desc 
+        res += self.coords
+        return     res   
+
+
+
+class Item(object):
+    """
+    Items / objects that are in the world. Can be collected
+    or crafted
+    """
+    def __init__(self, name, desc, spawns_at_locations):
+        self.name = name
+        self.desc = desc
+        self.spawns_at_locations =  spawns_at_locations
+    def __str__(self):
+        res = ''
+        res += self.name + ' - ' + self.desc + '\nSpawns at locations:'
+        for l in self.spawns_at_locations:
+            res += str(l.name) #+ ' = ' + l.spawn_rate + ' spawn chance'
+
+
+        return     res   
+
+
+class NPC(object):
+    """
+    a Non-Player character
+    """
+    def __init__(self, name, location, status, items_needed):
+        self.name = name
+        self.location = location
+        self.status = status
+        self.items_needed = items_needed
+    def __str__(self):
+        res = ''
+        res += self.name + ' is at ' + self.location.name + '. Status = ' + self.status
+        res += 'This NPC needs : '
+        #for i in self.items_needed:
+        #    res += str(i.name)
+        res += ', '.join([i.name for i in self.items_needed])
+
+        return     res   
+
+class Quest(object):
+    """
+    handles a single quest
+    """
+    def __init__(self):
+        pass
+    def __str__(self):
+        res = '+------------------------------------------------------------\n'
+        res += '| ***' + self.name + ' ***\n'
+        res += '| ' + self.desc + '\n'
+        res += '| Location = ' + str(self.location[0].name) + '\n'
+        res += '| Status = ' + self.status + '\n'
+        res += '| Reward = ' + self.reward + '\n'
+        res += '| Return to ' + self.quest_giver.name + ' with '
+        #res += ','.join([i.name for i in self.items_required])
+        res +=  str(self.quantity) + ' ' + self.items_required.name + '\n'
+        res += '+------------------------------------------------------------\n'
+
+        return     res   
+
+
+    def create_next_quest_via_npc_needs(self, npc):
+        """
+        takes NPC as parameter and finds the next quest this person needs
+        """
+
+        for needs in npc.items_needed:   # just the first one
+            self.name = 'Collect ' + needs.name
+            self.quest_giver = npc
+            self.quantity = random.choice([4,8,10,25])
+            self.reward = random.choice(['fishing rod', 'hammer', '5 Gold', '10 Gold'])
+            self.items_required = needs
+            self.desc = npc.name + ' needs you to collect ' + needs.name #+ '. You can find these at ' + str(needs.spawns_at_locations) 
+            self.status = 'Available'
+            self.location = needs.spawns_at_locations
+        
+        return self
 
 if __name__ == '__main__':
 	main()
