29136a21a11510e2ea5a1ef405c0ab5559f9c4e8
Duncan Murray <djmurray@gmail.com>
2020-04-03 00:04:31 +1030

    tests for quest gen to include location CSV

--- tests/test_quest_gen.py
+++ tests/test_quest_gen.py
@@ -22,14 +22,14 @@ class TestTemplate(unittest.TestCase):
         unittest.TestCase.tearDown(self)
 
 
-    def test_01_locations(self):
+    def test_01_location(self):
         hills = mod_quest.Location('hills', 'rolling hills', [4,8])
         print(hills)
         self.assertEqual(hills.name, 'hills')
         self.assertEqual(hills.desc, 'rolling hills')
         self.assertEqual(hills.coords, [4,8]) 
   
-    def test_02_items(self):
+    def test_02_item(self):
         hills = mod_quest.Location('hills', 'rolling hills', [4,8])
         forest = mod_quest.Location('forest', 'Dark forest near the hills', [4,7])
         woods = mod_quest.Location('woods', 'Woodland between mountains and river', [7,12])
@@ -38,6 +38,16 @@ class TestTemplate(unittest.TestCase):
         self.assertEqual(wood.name, 'wood')
         self.assertEqual(wood.desc, 'Wooden planks')
         self.assertEqual(wood.spawns_at_locations, [forest, hills, woods ]) 
+        
+
+    def test_03_locations(self):
+        locations = mod_quest.Locations()
+        locations.fill_from_csv('..\worldbuild\data\locations.csv')
+        print(locations)
+
+
+        self.assertEqual(len(str(locations)), 169) 
+        self.assertTrue(locations.raw_data[0], 'home,your home,0,0')
         
 
 
