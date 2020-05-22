d5196aa0b3ae2ef5a3ffef0ff7f10ced5ff5ba24
Duncan Murray <djmurray@gmail.com>
2020-05-23 00:08:25 +0930

    crafting test for DataSet and recipes

--- tests/test_crafting.py
+++ tests/test_crafting.py
@@ -24,9 +24,20 @@ class TestTemplate(unittest.TestCase):
 
     def test_01_recipe(self):
         res = mod_craft.Recipe('1', 'new recipe','20','mix')
-        print(res)
+        #print(res)
         self.assertEqual(str(res),'new recipe')
 
+    def test_02_dataset_recipe(self):
+        recipes = mod_craft.DataSet(mod_craft.Recipe, mod_craft.get_fullname('recipes.csv'))
+        self.assertEqual(str(recipes),'Dataset containing 6 Recipe objects')
+        tot_time_to_build = 0
+        for recipe in recipes.object_list:
+            #print(recipe)
+            tot_time_to_build += int(recipe.base_time_to_build)
+        #print('total time to build all recipes = ' +  str(tot_time_to_build))
+        self.assertEqual(str(recipes.object_list[0]), 'Torch')
+        self.assertEqual(str(recipes.object_list[1]), 'Wooden Plank')
+        self.assertEqual(19, tot_time_to_build)
     
 
 
