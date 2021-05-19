946d8678929d73a0c3372932797044bbe4c54102
Duncan Murray <djmurray@gmail.com>
2021-05-19 20:29:02 +0930

    added attributes to npc generation and describe function

--- worldbuild/npc_gen/npc_gen.py
+++ worldbuild/npc_gen/npc_gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# town_gen.py
+# npc_gen.py
 
 import os 
 import sys
@@ -28,9 +28,13 @@ locations = [  # name,desc,coord_x,coord_y
 ['old_house','Abandoned House',5,1 ],
 ]
 
+attitudes = [[-9,'nasty'], [-5,'dismissmive'], [0,'neutral'], [5,'helpful'], [9, 'caring']]
+professions = ['cook', 'farmer', 'builder', 'vet', 'woodworker', 'helper']
+childhoods = ['poor', 'normal', 'privileged']
+
 def TEST():
     print('generating NPC list...')
-    res = make_npcs(20)
+    res = make_npcs(5)
     save_list_to_csv(res, 'random_npc.csv')
     print("Done")
 
@@ -39,15 +43,44 @@ def make_npcs(num_npcs):
     generates the NPC list
     """
     list_names = generate.get_list_people()
-    npcs = [['npc_name', 'age', 'location']]
+    npcs = [['npc_name', 'age', 'location', 'born', 'attitude', 'profession', 'work_location', 'childhood']]
     for i in range(1, num_npcs):
         nme = random.choice(list_names)
         age = random.randint(16,60)
-        location = random.choice(locations[1:])[0]
-
-        npcs.append([nme, age, location])
+        location = random.choice(locations[1:])[1]
+        born = random.choice(generate.get_list_places())
+        attitude = random.choice(attitudes)[1]
+        profession = random.choice(professions)
+        work_location = random.choice(locations[1:])[1]
+        childhood = random.choice(childhoods)
+        this_npc = [nme, age, location, born, attitude, profession, work_location, childhood]
+        describe_npc(this_npc)
+        npcs.append(this_npc)
 
     return npcs
+
+
+def describe_npc(npc_spec):
+    """
+    using the spec and reference tables, gives a short narrative about an NPC
+    """
+    #print('npc spec for ....' + str(npc_spec))
+    nme = npc_spec[0]
+    age = str(npc_spec[1])
+    location = npc_spec[2] 
+    born = npc_spec[3] 
+    pers = npc_spec[4] 
+    prof = npc_spec[5] 
+    work = npc_spec[6] 
+    childhood = npc_spec[7] 
+    res = ''
+    res += '------------------------------------------------\n'
+    res += nme + ' was born ' + age + ' years ago in ' + born + '\n'
+    res += 'They work as ' + prof + ' in ' + work + '\n'
+    res += nme + ' had a ' + childhood + ' childhood and have a ' + pers + ' personality.'
+
+    print(res)
+    
     
 
 def save_list_to_csv(lst, filename):
